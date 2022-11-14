47d22960fec25980d7490a1d21fef7a5b9c7ceb7
chtenu <ch.tenu11@gmail.com>
2022-11-14 19:15:43 +0530

    first commit

---
+++ inx.js
@@ -0,0 +1,30 @@
+let countEl=document.getElementById("count-el")
+let imScore=0
+ function homeSco() {
+    imScore=imScore+1
+    countEl.innerText=imScore
+    //console.log(imScore)
+ }
+ function homeSco2(){
+     imScore=imScore+2
+     countEl.innerText=imScore
+ }
+ function homeSco3(){
+     imScore=imScore+3
+     countEl.innerText=imScore
+ }
+ let guestEl=document.getElementById("guest-el")
+ let guScore=0
+ function gueSco() {
+    guScore=guScore+1
+    guestEl.innerText=guScore
+}
+ function gueSco2() {
+    guScore=guScore+2
+    guestEl.innerText=guScore
+}
+ function gueSco3() {
+    guScore=guScore+3
+    guestEl.innerText=guScore
+}
+ 
